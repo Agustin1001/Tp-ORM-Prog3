f278f57580759e78e7d34a9af4bb7b010629d3e6
juakkoo <juaakkoo@gmail.com>
2025-08-19 18:10:10 -0300

    Cambio joaco

--- src/main/java/org/example/DetalleHistoriaClinica.java
+++ src/main/java/org/example/DetalleHistoriaClinica.java
@@ -1,4 +1,81 @@
 package org.example;
 
+import jakarta.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "detalle_historia_clinica")
 public class DetalleHistoriaClinica {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idDetalleHC;
+
+    @Temporal(TemporalType.DATE)
+    private Date fechaAtencion;
+
+    private String sintomas;
+    private String diagnostico;
+    private String observaciones;
+
+    @ManyToOne
+    @JoinColumn(name = "id_historia_clinica", nullable = false)
+    private HistoriaClinica historiaClinica;
+
+    public DetalleHistoriaClinica() {}
+
+    public DetalleHistoriaClinica(Date fechaAtencion, String sintomas, String diagnostico, String observaciones) {
+        this.fechaAtencion = fechaAtencion;
+        this.sintomas = sintomas;
+        this.diagnostico = diagnostico;
+        this.observaciones = observaciones;
+    }
+
+    public int getIdDetalleHC() {
+        return idDetalleHC;
+    }
+
+    public void setIdDetalleHC(int idDetalleHC) {
+        this.idDetalleHC = idDetalleHC;
+    }
+
+    public Date getFechaAtencion() {
+        return fechaAtencion;
+    }
+
+    public void setFechaAtencion(Date fechaAtencion) {
+        this.fechaAtencion = fechaAtencion;
+    }
+
+    public String getSintomas() {
+        return sintomas;
+    }
+
+    public void setSintomas(String sintomas) {
+        this.sintomas = sintomas;
+    }
+
+    public String getDiagnostico() {
+        return diagnostico;
+    }
+
+    public void setDiagnostico(String diagnostico) {
+        this.diagnostico = diagnostico;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+
+    public HistoriaClinica getHistoriaClinica() {
+        return historiaClinica;
+    }
+
+    public void setHistoriaClinica(HistoriaClinica historiaClinica) {
+        this.historiaClinica = historiaClinica;
+    }
 }
--- src/main/java/org/example/HistoriaClinica.java
+++ src/main/java/org/example/HistoriaClinica.java
@@ -1,4 +1,73 @@
 package org.example;
 
+import jakarta.persistence.*;
+import java.util.*;
+
+@Entity
+@Table(name = "historia_clinica")
 public class HistoriaClinica {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idHistoriaClinica;
+
+    private int numero;
+
+    @Temporal(TemporalType.DATE)
+    private Date fechaAlta;
+
+    @OneToOne
+    @JoinColumn(name = "id_paciente", nullable = false, unique = true)
+    private Paciente paciente;
+
+    @OneToMany(mappedBy = "historiaClinica", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<DetalleHistoriaClinica> detalles = new ArrayList<>();
+
+    public HistoriaClinica() {}
+
+    public HistoriaClinica(int numero, Date fechaAlta, Paciente paciente) {
+        this.numero = numero;
+        this.fechaAlta = fechaAlta;
+        this.paciente = paciente;
+    }
+
+    public int getIdHistoriaClinica() {
+        return idHistoriaClinica;
+    }
+
+    public void setIdHistoriaClinica(int idHistoriaClinica) {
+        this.idHistoriaClinica = idHistoriaClinica;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public Date getFechaAlta() {
+        return fechaAlta;
+    }
+
+    public void setFechaAlta(Date fechaAlta) {
+        this.fechaAlta = fechaAlta;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    public List<DetalleHistoriaClinica> getDetalles() {
+        return detalles;
+    }
+
+    public void setDetalles(List<DetalleHistoriaClinica> detalles) {
+        this.detalles = detalles;
+    }
 }
