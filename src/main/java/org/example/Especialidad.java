8840540f1371042a7ca56901a9727522baf763ee
Carlosfmr95 <munozramoscarlos@gmail.com>
2025-08-19 18:08:11 -0300

    Parte Carlos

--- src/main/java/org/example/Especialidad.java
+++ src/main/java/org/example/Especialidad.java
@@ -1,4 +1,69 @@
 package org.example;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name= "especialidades")
 public class Especialidad {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idEspecialidad;
+
+    @Column(nullable = false, length = 100)
+    private String denominacion;
+
+    @ManyToMany(mappedBy = "especialidades")
+    private List<Medico> listaMedicos = new ArrayList<>();
+
+    public Especialidad() {
+    }
+
+    public Especialidad(int idEspecialidad, String denominacion) {
+        this.idEspecialidad = idEspecialidad;
+        this.denominacion = denominacion;
+    }
+
+    public Especialidad(int idEspecialidad, String denominacion, Medico medico) {
+        this.idEspecialidad = idEspecialidad;
+        this.denominacion = denominacion;
+        this.listaMedicos.add(medico);
+    }
+
+    public int getIdEspecialidad() {
+        return idEspecialidad;
+    }
+
+    public void setIdEspecialidad(int idEspecialidad) {
+        this.idEspecialidad = idEspecialidad;
+    }
+
+    public String getDenominacion() {
+        return denominacion;
+    }
+
+    public void setDenominacion(String denominacion) {
+        this.denominacion = denominacion;
+    }
+
+    public List<Medico> getListaMedicos() {
+        return listaMedicos;
+    }
+
+    public void setListaMedicos(List<Medico> listaMedicos) {
+        this.listaMedicos = listaMedicos;
+    }
+
+    public void agregarMedico(Medico medico) {
+        this.listaMedicos.add(medico);
+    }
+
+    public void eliminarMedico(Medico medico) {
+        if(this.listaMedicos != null) {
+            this.listaMedicos.remove(medico);
+        }
+    }
 }
--- src/main/java/org/example/Turno.java
+++ src/main/java/org/example/Turno.java
@@ -1,4 +1,97 @@
 package org.example;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "turnos")
 public class Turno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idTurno;
+
+    @Temporal(TemporalType.DATE)
+    private Date fecha;
+
+    private int hora;
+
+    private int minutos;
+
+
+    @ManyToOne
+    @JoinColumn(name = "id_medico")
+    private Medico medico;
+
+    @ManyToOne
+    @JoinColumn(name = "id_paciente")
+    private Paciente paciente;
+
+    public Turno() {
+    }
+
+    public Turno(int idTurno, Date fecha, int hora, int minutos) {
+        this.idTurno = idTurno;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.minutos = minutos;
+    }
+
+    public Turno(int idTurno, Date fecha, int hora, int minutos, Medico medico, Paciente paciente) {
+        this.idTurno = idTurno;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.minutos = minutos;
+        this.medico = medico;
+        this.paciente = paciente;
+    }
+
+    public int getIdTurno() {
+        return idTurno;
+    }
+
+    public void setIdTurno(int idTurno) {
+        this.idTurno = idTurno;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getHora() {
+        return hora;
+    }
+
+    public void setHora(int hora) {
+        this.hora = hora;
+    }
+
+    public int getMinutos() {
+        return minutos;
+    }
+
+    public void setMinutos(int minutos) {
+        this.minutos = minutos;
+    }
+
+    public Medico getMedico() {
+        return medico;
+    }
+
+    public void setMedico(Medico medico) {
+        this.medico = medico;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
 }
